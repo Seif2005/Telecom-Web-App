022cea8bf37d37663abdf5474eb319d39a8544d4
seif <seif.m05@gmail.com>
2024-12-03 14:15:39 +0200

    Remove benefit

---
+++ RemovePlanBenefits.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RemovePlanBenefits.aspx.cs" Inherits="Telecom_Web_App.RemovePlanBenefits" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Remove benefits</h2>
+            <asp:Label ID="lblMobileNo" runat="server" Text="Mobile Number:"></asp:Label>
+            <asp:TextBox ID="txtMobileno" runat="server"></asp:TextBox>
+            <br /><br />
+            <asp:Label ID="lblPlanId" runat="server" Text=" Plan Id:"></asp:Label>
+            <asp:TextBox ID="txtPlanID" runat="server" ></asp:TextBox>
+            <br /><br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Remove" OnClick="RemoveClick" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ RemovePlanBenefits.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static Telecom_Web_App.Helpers;
+
+namespace Telecom_Web_App
+{
+    public partial class RemovePlanBenefits : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void RemoveClick(object sender, EventArgs e)
+        {
+            //Benefits_Account 
+            String mobile = txtMobileno.Text;
+            String planId = txtPlanID.Text;
+            DataTable Data = new DataTable();
+            string connStr = WebConfigurationManager.ConnectionStrings["dbConnection"].ToString();
+
+            using (SqlConnection connection = new SqlConnection(connStr))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("Benefits_Account", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@mobile_num", mobile);
+                    command.Parameters.AddWithValue("@plan_id", int.Parse(planId));
+                    command.ExecuteNonQuery();
+                }
+                Data = GetData("SELECT * FROM Benefits");
+            }
+
+            Session["GridData"] = Data;
+            Response.Redirect("/Result.aspx");
+        }
+
+    }
+}
