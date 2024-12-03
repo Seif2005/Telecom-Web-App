a614ac15b7682411d6ea69ab64a553267c2e65e9
Youssef Mostafa <youssefmostafa2372004@gmail.com>
2024-12-03 19:16:28 +0200

    cashback_wallet_planid and Avg_SentTrans_From_wallet_Date

---
+++ Avg_SentTrans_From_wallet_Date.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Avg_SentTrans_From_wallet_Date.aspx.cs" Inherits="Telecom_Web_App.Avg_SentTrans_From_wallet_Date" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+       <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Wallet Id and date Specification  </h2>
+            <asp:Label ID="lblWalletId" runat="server" Text="Wallet ID: "></asp:Label>
+            <asp:TextBox ID="txtWalletId" runat="server"></asp:TextBox>
+            <br /><br />
+            <asp:Label ID="lblTime" runat="server" Text=" From Date:"></asp:Label>
+            <asp:TextBox ID="txtStartTime" runat="server" TextMode="Date"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="lblTime0" runat="server" Text=" To Date:"></asp:Label>
+            <asp:TextBox ID="txtEndTime" runat="server" TextMode="Date"></asp:TextBox>
+            <br /><br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Get Average Sent Transactions" OnClick="Get_Average_Sent_Transactions" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Avg_SentTrans_From_wallet_Date.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Telecom_Web_App
+{
+    public partial class Avg_SentTrans_From_wallet_Date : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Get_Average_Sent_Transactions(object sender, EventArgs e)
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["dbConnection"].ToString();
+            if (string.IsNullOrEmpty(txtWalletId.Text) || string.IsNullOrEmpty(txtStartTime.Text)|| string.IsNullOrEmpty(txtEndTime.Text))
+            {
+                Response.Write("please Enter a wallet id ,a Start Date and a End Date");
+            }
+            else
+            {
+                int Wallet_ID = int.Parse(txtWalletId.Text);
+                String Start_Date = txtStartTime.Text;
+                String End_Date = txtEndTime.Text;
+                using (SqlConnection conn = new SqlConnection(connStr)) 
+                {
+                    conn.Open();
+                    using (SqlCommand command = new SqlCommand("SELECT dbo.Wallet_Transfer_Amount(@walletID , @start_date , @end_date )", conn))
+                    {
+                        
+                        command.Parameters.AddWithValue("@walletID ", Wallet_ID);
+                        command.Parameters.AddWithValue("@start_date", Start_Date);
+                        command.Parameters.AddWithValue("@end_date", End_Date);
+                        object result = command.ExecuteScalar();
+                        if (result.ToString() != "")
+                        {
+                            int TransferAmount = int.Parse(result.ToString());
+                            Response.Write("the Transfer Amount is " + TransferAmount.ToString());
+                        }
+                        else
+                        {
+                            Response.Write("please Enter a Valid wallet id and a Plan id");
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
